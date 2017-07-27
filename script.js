37afdc054814a944d811791d9ededa2cc7850321
hemme1m <markhemmert@gmail.com>
2017-07-27 15:58:28 -0400

    cleaned up comments in html and script files, deleted some unnecessary code

--- script.js
+++ script.js
@@ -7,17 +7,16 @@ $(".box").click(function() {
 	if ($(this).attr("title") === "This seat is available!") {
 		reservedSeat = $(this).attr("id");			//saves clicked box's id to variable reservedSeat
 		$("#reservationModal").modal("show");		//shows modal for reservation form
-		$("#inputName").focus();					//sets focus to name input
 	}
 });
 
 $("#submitBtn").click(function() {
-	$("#" + reservedSeat).css("backgroundColor", "red").addClass("reserved");	//changes the color of the reserved seat box
+	$("#" + reservedSeat).css("backgroundColor", "red")	//upon submission, changes the color of the reserved seat box
 	reservations.push({name: $("#inputName").val(), email: $("#inputEmail").val(), seat: reservedSeat.toUpperCase()}) //adds an object to the reservations array with user info
 	$("#inputName").val("");					//reset name field
 	$("#inputEmail").val("");					//reset email field
 	$("reservationsModal").modal("hide");		//hides modal after user submits reservation info
-	console.log(reservations);
+	console.log(reservations);					//per David's request, logs reservations array to console
 });
 
 $(".box").mouseover(function() {
@@ -26,7 +25,7 @@ $(".box").mouseover(function() {
 	// for loop runs through reservations array, and if user hovers over a reserved seat, it will match that seat id and output the reservation information
 	for (var i = 0; i < reservations.length; i++) {
 		if(hoveredSeat === reservations[i].seat) {		//while for loop is running, checks to see if there is a match between hovered seat id and seat id for an object in the array
-			$(this).attr("title" , reservations[i].name + ", " + reservations[i].email + ", " + reservations[i].seat );
+			$(this).attr("title" , reservations[i].name + ", " + reservations[i].email + ", " + reservations[i].seat ); //if seat is reserved, this changes the title for that element to user name, email and seat number
 		}
 	}
 });
