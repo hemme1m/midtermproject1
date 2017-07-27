0e21f190980b577f60ca45535dc74327268b9370
hemme1m <markhemmert@gmail.com>
2017-07-27 10:59:29 -0400

    adjusted sizes on pages, fixed hover function

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-//$(function() {
-// on box click, ticket image disappears and form appear
+$(function() {
 var reservations = [];
 var reservedSeat = "";
 var hoveredSeat = "";
 
 $(".box").click(function() {
-	$("#ticketImg").css("display", "none");		//hides the ticket image
-	$("#resForm").css("display", "block");		//displays reservation form
-	reservedSeat = $(this).attr("id");
+	reservedSeat = $(this).attr("id");			//saves clicked box's id to variable reservedSeat
+	$("#reservationModal").modal("show");		//shows modal for reservation form
 	$("#inputName").focus();					//sets focus to name input
 	
 });
 
-$("button").click(function() {
-	$("#ticketImg").css("display", "block");	//displays ticket image
-	$("#resForm").css("display", "none");		//hides reservation form
+$("#submitBtn").click(function() {
 	$("#" + reservedSeat).css("backgroundColor", "red").addClass("reserved");	//changes the color of the reserved seat box
 	reservations.push({name: $("#inputName").val(), email: $("#inputEmail").val(), seat: reservedSeat}) //adds an object to the reservations array with user info
 	$("#inputName").val("");					//reset name field
 	$("#inputEmail").val("");					//reset email field
+	$("reservationsModal").modal("hide");		//hides modal after user submits reservation info
 });
 
 $(".box").mouseover(function() {
-	hoveredSeat = $(this).attr("id");
+	hoveredSeat = $(this).attr("id");			//save seat id to variable when user hovers over a box
 
+	// for loop runs through reservations array, and if user hovers over a reserved seat, it will match that seat id and output the reservation information
 	for (var i = 0; i < reservations.length; i++) {
-		if(hoveredSeat === reservations[i].seat) {
-			// $("#output").html("<p>Reservation Information:" + "<br>" + "Name: " + reservations[i].name 
-			//  	+ "<br>" + "Email: " + reservations[i].email + "</p>");
-			console.log("Wow!");
+		if(hoveredSeat === reservations[i].seat) {		//while for loop is running, checks to see if there is a match between hovered seat id and seat id for an object in the array
+			$("#output").html("<br><br><p>Reservation Information:" + "<br>" + "Name: " + reservations[i].name 
+			  	+ "<br>" + "Email: " + reservations[i].email + "<br>" + "Seat Number: " + reservations[i].seat + "</p>");		//if the hovered seat id finds a match, that user's reservation info shows up on page below the seats
 		}
 	}
-	//$("#output").html("Reservation Information:" + <br> + "Name: " + )
 });
 
-
-//});
+});
